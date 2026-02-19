533f14ad2c5a25700716ca195c281fbd1ad648b9
andrealangianese <andre.langia.01@gmail.com>
2026-02-19 14:42:57 +0100

    importo il db nella cartella data con le credenziali

---
+++ data/db-new.js
@@ -0,0 +1,20 @@
+//importo sql
+
+const mysql = require('mysql2');
+
+//creo connessione che mi servirà dopo con le mie credenziali
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'blog'
+});
+
+// stampo per vedere se hoqualche errore
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL!');
+});
+module.exports = connection;
